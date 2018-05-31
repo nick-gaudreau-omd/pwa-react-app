0703997e7fa587e82538021f17a34c130597d4fd
Nick Gaudreau <yannickgaudreau@gmail.com>
2018-05-30 23:07:24 -0400

    Added basic admin dashboard, route and style

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import Survey from './pages/survey/Survey';
 import NewsContent from './pages/news/Content';
 import NewsContainer from './pages/news/Container';
+import Dashboard from './pages/admin/Dashboard';
 
 class Routes extends React.Component<{ }, {}> {
 
@@ -16,6 +17,7 @@ class Routes extends React.Component<{ }, {}> {
                     <Route path='/news/:category' component={NewsContainer} />
                     <Route path='/content/:value' component={NewsContent} />
                     <Route path='/survey' component={Survey} />
+                    <Route path='/admin' component={Dashboard} />
                     <Route path='/' component={NewsContainer} />
                     <Redirect to="/" />
                 </Switch>
---
+++ src/pages/admin/DashboardNav.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import "./dashboard.css";
+
+export default class DashboardNav extends React.Component<{}, {}> {
+
+
+    public render() {
+
+        return (
+            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="/">PWA Offline News</a>
+                <input className="form-control form-control-dark w-100" type="text" placeholder="Search" />
+                <ul className="navbar-nav px-3">
+                    <li className="nav-item text-nowrap">
+                        <a className="nav-link" href="/">Sign out</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
